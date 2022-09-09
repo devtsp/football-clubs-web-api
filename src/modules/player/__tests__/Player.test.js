5408fdac369a55b1e994e5732600581d06bdf6d1
Tomas <paeztms@gmail.com>
2022-09-09 01:35:32 -0300

    add field type checking to Player constructor and tests

--- src/modules/player/__tests__/Player.test.js
+++ src/modules/player/__tests__/Player.test.js
@@ -1,71 +1,91 @@
 const Player = require('../entity/Player');
 
 const fakeFields = {
-	playerFirstName: 'Connor',
-	playerLastName: 'McGregor',
-	palyerAge: 28,
-	playerPosition: 9,
+  playerFirstName: 'Connor',
+  playerLastName: 'McGregor',
+  playerAge: 28,
+  playerPosition: 9,
 };
 
 describe('>> Player', () => {
-	test('Throws on totally empty/nullish fields', () => {
-		expect(() => new Player({})).toThrow('Missing some required fields');
-		expect(() => new Player(null)).toThrow(
-			'Must provide a valid object to initialize entity'
-		);
-		expect(() => new Player(undefined)).toThrow(
-			'Must provide a valid object to initialize entity'
-		);
-		expect(() => new Player([])).toThrow(
-			'Must provide a valid object to initialize entity'
-		);
-		expect(() => new Player(123)).toThrow(
-			'Must provide a valid object to initialize entity'
-		);
-		expect(() => new Player('')).toThrow(
-			'Must provide a valid object to initialize entity'
-		);
-	});
+  test('Throws if not object passed as an argument', () => {
+    expect(() => new Player(null)).toThrow(
+      'Must provide a valid object to initialize entity'
+    );
 
-	test('Autogenerates an Id', () => {
-		expect(new Player(fakeFields).getId()).toBeDefined();
-	});
+    expect(() => new Player(undefined)).toThrow(
+      'Must provide a valid object to initialize entity'
+    );
 
-	test('firstname and lastname must be of type "string"', () => {
-		expect(() => new Player({ ...fakeFields, playerFirstName: null })).toThrow(
-			'Player firstname and lastname must be of type: string'
-		);
-		expect(
-			() => new Player({ ...fakeFields, playerFirstName: undefined })
-		).toThrow('Player firstname and lastname must be of type: string');
-		expect(() => new Player({ ...fakeFields, playerFirstName: 123 })).toThrow(
-			'Player firstname and lastname must be of type: string'
-		);
+    expect(() => new Player([])).toThrow(
+      'Must provide a valid object to initialize entity'
+    );
 
-		expect(() => new Player({ ...fakeFields, playerLastName: null })).toThrow(
-			'Player firstname and lastname must be of type: string'
-		);
-		expect(
-			() => new Player({ ...fakeFields, playerLastName: undefined })
-		).toThrow('Player firstname and lastname must be of type: string');
-		expect(() => new Player({ ...fakeFields, playerLastName: 123 })).toThrow(
-			'Player firstname and lastname must be of type: string'
-		);
-	});
-	test('Must have a first name and lastname (no empty strings)', () => {
-		expect(() => new Player({ ...fakeFields, playerFirstName: '' })).toThrow(
-			'Player must have firstname and lastname'
-		);
-		expect(() => new Player({ ...fakeFields, playerLastName: '' })).toThrow(
-			'Player must have firstname and lastname'
-		);
-	});
-	test('Must have a valid first name and lastname (only alphabetic chars)', () => {
-		expect(() => new Player({ ...fakeFields, playerFirstName: '123' })).toThrow(
-			'Invalid firstname'
-		);
-		expect(
-			() => new Player({ ...fakeFields, playerLastName: '23523' })
-		).toThrow('Invalid lastname');
-	});
+    expect(() => new Player(123)).toThrow(
+      'Must provide a valid object to initialize entity'
+    );
+
+    expect(() => new Player('')).toThrow(
+      'Must provide a valid object to initialize entity'
+    );
+
+    expect(() => new Player(true)).toThrow(
+      'Must provide a valid object to initialize entity'
+    );
+  });
+
+  test('Throws if not valid key present ', () => {
+    expect(
+      () => new Player({ someRandomProp: false, anotherNotRequiredProp: [] })
+    ).toThrow('No valid property was found to start player creation');
+  });
+
+  test('Autogenerates an Id', () => {
+    expect(new Player(fakeFields).getId()).toBeDefined();
+  });
+
+  test('firstname and lastname must be of type "string"', () => {
+    expect(() => new Player({ ...fakeFields, playerFirstName: null })).toThrow(
+      'Player firstname and lastname must be of type: string'
+    );
+
+    expect(
+      () => new Player({ ...fakeFields, playerFirstName: undefined })
+    ).toThrow('Player firstname and lastname must be of type: string');
+
+    expect(() => new Player({ ...fakeFields, playerFirstName: 123 })).toThrow(
+      'Player firstname and lastname must be of type: string'
+    );
+
+    expect(() => new Player({ ...fakeFields, playerLastName: null })).toThrow(
+      'Player firstname and lastname must be of type: string'
+    );
+
+    expect(
+      () => new Player({ ...fakeFields, playerLastName: undefined })
+    ).toThrow('Player firstname and lastname must be of type: string');
+
+    expect(() => new Player({ ...fakeFields, playerLastName: 123 })).toThrow(
+      'Player firstname and lastname must be of type: string'
+    );
+  });
+
+  test('Must have a first name and lastname (no empty strings)', () => {
+    expect(() => new Player({ ...fakeFields, playerFirstName: '' })).toThrow(
+      'Player must have firstname and lastname'
+    );
+
+    expect(() => new Player({ ...fakeFields, playerLastName: '' })).toThrow(
+      'Player must have firstname and lastname'
+    );
+  });
+
+  test('Must have a valid first name and lastname (only alphabetic chars)', () => {
+    expect(() => new Player({ ...fakeFields, playerFirstName: '123' })).toThrow(
+      'Invalid firstname'
+    );
+    expect(
+      () => new Player({ ...fakeFields, playerLastName: '23523' })
+    ).toThrow('Invalid lastname');
+  });
 });
--- src/modules/player/entity/Player.js
+++ src/modules/player/entity/Player.js
@@ -1,64 +1,86 @@
 const IdService = require('../../../helpers/generate-id.helper');
 
 module.exports = class Player {
-	constructor(playerFieldsRecieved) {
-		const playerProps = [
-			'playerFirstName',
-			'playerLastName',
-			'playerAge',
-			'playerPosition',
-		];
+  constructor(playerFieldsRecieved) {
+    const playerProps = [
+      'playerFirstName',
+      'playerLastName',
+      'playerAge',
+      'playerPosition',
+    ];
 
-		if (typeof playerFieldsRecieved.constructor.name !== 'Object') {
-			throw new Error('Must provide a valid object to initialize entity');
-		}
+    // TO RECIEVE: OBJECT
+    if (
+      typeof playerFieldsRecieved == 'undefined' ||
+      playerFieldsRecieved?.constructor?.name != 'Object'
+    ) {
+      throw new Error('Must provide a valid object to initialize entity');
+    }
 
-		const validFields = Object.keys(playerFieldsRecieved).map(fieldRecieved =>
-			playerProps.includes(fieldRecieved)
-		);
+    const validFields = Object.keys(playerFieldsRecieved).filter(
+      (fieldRecieved) => playerProps.includes(fieldRecieved)
+    );
 
-		if (!validFields.length) {
-			throw new Error('No valid property was found to start player creation');
-		}
+    // RECIEVED HAS AT LEAST ONE VALID PROP
+    if (!validFields.length) {
+      throw new Error('No valid property was found to start player creation');
+    }
 
-		if (
-			!(typeof playerFirstName == 'string') ||
-			!(typeof playerLastName == 'string')
-		) {
-			throw new Error('Player firstname and lastname must be of type: string');
-		}
+    const {
+      playerFirstName,
+      playerLastName,
+      playerAge,
+      playerPosition,
+      clubId,
+    } = playerFieldsRecieved;
 
-		if (!playerFirstName || !playerLastName) {
-			throw new Error('Player must have firstname and lastname');
-		}
-		if (/[^A-Za-z]/.test(playerFirstName)) {
-			throw new Error('Invalid firstname');
-		}
-		if (/[^A-Za-z]/.test(playerLastName)) {
-			throw new Error('Invalid lastname');
-		}
-		if (playerAge < 16 || playerAge > 45) {
-			throw new Error('Player must be between 16 and 45 years old ');
-		}
-		if (playerPosition < 1 || playerPosition > 11) {
-			throw new Error('Positions available for a player go between 1 and 11');
-		}
+    // NAME AND LASTNAME: STRING NOT EMPTY
+    if (
+      typeof playerFirstName != 'string' ||
+      typeof playerLastName != 'string'
+    ) {
+      throw new Error('Player firstname and lastname must be of type: string');
+    }
+    if (!playerFirstName.trim() || !playerLastName.trim()) {
+      throw new Error('Player must have firstname and lastname');
+    }
 
-		this.playerFirstName = playerFirstName;
-		this.playerLastName = playerLastName;
-		this.playerAge = playerAge;
-		this.playerPosition = playerPosition;
-		this.clubId = clubId;
-		this.playerId = IdService.generateId();
-		this.timeStamp = Date.now();
-	}
+    // NAME AND LASTNAME: VALID
+    if (/[^A-Za-z]/.test(playerFirstName)) {
+      throw new Error('Invalid firstname');
+    }
+    if (/[^A-Za-z]/.test(playerLastName)) {
+      throw new Error('Invalid lastname');
+    }
 
-	getId = () => this.playerId;
-	getFirstName = () => this.playerFirstName;
-	getLastName = () => this.playerLastName;
-	getAge = () => this.playerAge;
-	getPosition = () => this.playerPosition;
-	getPClubId = () => this.clubId;
-	getCreatedAt = () => this.timeStamp;
-	getUpdatedAt = () => this.timeStamp;
+    // AGE AND POSITION: NUMBERS
+    if (typeof playerAge != 'number' || typeof playerPosition != 'number') {
+      throw new Error('Player age and position must be of type: number');
+    }
+
+    // AGE AND POSITION: VALID
+    if (playerAge < 16 || playerAge > 45) {
+      throw new Error('Player must be between 16 and 45 years old ');
+    }
+    if (playerPosition < 1 || playerPosition > 11) {
+      throw new Error('Positions available for a player go between 1 and 11');
+    }
+
+    this.playerFirstName = playerFirstName;
+    this.playerLastName = playerLastName;
+    this.playerAge = playerAge;
+    this.playerPosition = playerPosition;
+    this.clubId = clubId || null;
+    this.playerId = IdService.generateId();
+    this.timeStamp = Date.now();
+  }
+
+  getId = () => this.playerId;
+  getFirstName = () => this.playerFirstName;
+  getLastName = () => this.playerLastName;
+  getAge = () => this.playerAge;
+  getPosition = () => this.playerPosition;
+  getPClubId = () => this.clubId;
+  getCreatedAt = () => this.timeStamp;
+  getUpdatedAt = () => this.timeStamp;
 };
